9c1bb8806e1ba6a5c210a1f4113eb483d7e5da0f
lumskii <lumi8866@gmail.com>
2022-04-20 16:28:46 -0700

    Third commit 	modified:   index.html 	new file:   scripts.js 	new file:   styles.css GNU bash, version 4.4.23(2)-release (x86_64-pc-msys) These shell commands are defined internally.  Type `help' to see this list. Type `help name' to find out more about the function `name'. Use `info bash' to find out more about the shell in general. Use `man -k' or `info' to find out more about commands not in this list.
    A star (*) next to a name means that the command is disabled.
    
     job_spec [&]                            history [-c] [-d offset] [n] or hist>
     (( expression ))                        if COMMANDS; then COMMANDS; [ elif C>
     . filename [arguments]                  jobs [-lnprs] [jobspec ...] or jobs >
     :                                       kill [-s sigspec | -n signum | -sigs>
     [ arg... ]                              let arg [arg ...]
     [[ expression ]]                        local [option] name[=value] ...
     alias [-p] [name[=value] ... ]          logout [n]
     bg [job_spec ...]                       mapfile [-d delim] [-n count] [-O or>
     bind [-lpsvPSVX] [-m keymap] [-f file>  popd [-n] [+N | -N]
     break [n]                               printf [-v var] format [arguments]
     builtin [shell-builtin [arg ...]]       pushd [-n] [+N | -N | dir]
     caller [expr]                           pwd [-LPW]
     case WORD in [PATTERN [| PATTERN]...)>  read [-ers] [-a array] [-d delim] [->
     cd [-L|[-P [-e]] [-@]] [dir]            readarray [-n count] [-O origin] [-s>
     command [-pVv] command [arg ...]        readonly [-aAf] [name[=value] ...] o>
     compgen [-abcdefgjksuv] [-o option] [>  return [n]
     complete [-abcdefgjksuv] [-pr] [-DE] >  select NAME [in WORDS ... ;] do COMM>
     compopt [-o|+o option] [-DE] [name ..>  set [-abefhkmnptuvxBCHP] [-o option->
     continue [n]                            shift [n]
     coproc [NAME] command [redirections]    shopt [-pqsu] [-o] [optname ...]
     declare [-aAfFgilnrtux] [-p] [name[=v>  source filename [arguments]
     dirs [-clpv] [+N] [-N]                  suspend [-f]
     disown [-h] [-ar] [jobspec ... | pid >  test [expr]
     echo [-neE] [arg ...]                   time [-p] pipeline
     enable [-a] [-dnps] [-f filename] [na>  times
     eval [arg ...]                          trap [-lp] [[arg] signal_spec ...]
     exec [-cl] [-a name] [command [argume>  true
     exit [n]                                type [-afptP] name [name ...]
     export [-fn] [name[=value] ...] or ex>  typeset [-aAfFgilnrtux] [-p] name[=v>
     false                                   ulimit [-SHabcdefiklmnpqrstuvxPT] [l>
     fc [-e ename] [-lnr] [first] [last] o>  umask [-p] [-S] [mode]
     fg [job_spec]                           unalias [-a] name [name ...]
     for NAME [in WORDS ... ] ; do COMMAND>  unset [-f] [-v] [-n] [name ...]
     for (( exp1; exp2; exp3 )); do COMMAN>  until COMMANDS; do COMMANDS; done
     function name { COMMANDS ; } or name >  variables - Names and meanings of so>
     getopts optstring name [arg]            wait [-n] [id ...]
     hash [-lr] [-p pathname] [-dt] [name >  while COMMANDS; do COMMANDS; done
     help [-dms] [pattern ...]               { COMMANDS ; }

---
+++ scripts.js
@@ -0,0 +1,121 @@
+//declaring variables using the DOM
+const boxes = document.querySelectorAll('.box');
+const text = document.querySelector('#heading');
+const strategy = document.querySelector('#strategy');
+const restartBtn = document.querySelector('#restart');
+
+//Drawing the Board using a forEach function
+const drawBoard = () => {
+    boxes.forEach((box, i) => {
+        let styleString = '';
+        if (i < 3) {
+            styleString += 'border-bottom: 3px solid var(--text);';
+        }
+        if (i % 3 === 0) {
+            styleString += 'border-right: 3px solid var(--text);';
+        }
+        if (i % 3 === 2) {
+            styleString += 'border-left: 3px solid var(--text);';
+        }
+        if (i > 5) {
+            styleString += 'border-top: 3px solid var(--text);';
+        }
+        box.style = styleString;
+        box.addEventListener('click', boxClicked);
+    });
+};
+
+//Defining variable for the O's and X's
+const spaces = [];
+const tick_circle = 'O';
+const tick_x = 'X';
+let currentPlayer = tick_circle;
+
+//The boxClicked function taking the click event as an argument
+const boxClicked = (e) => {
+    const id = e.target.id;
+    if (!spaces[id]) {
+        spaces[id] = currentPlayer;
+        e.target.innerText = currentPlayer;
+
+        if (playerWon()) {
+            text.innerText = `${currentPlayer} has won!`;
+            restart();
+            return;
+        }
+
+        if (playerDraw()) {
+            return;
+        }
+        currentPlayer = currentPlayer === tick_circle ? tick_x : tick_circle;
+    }
+};
+
+//Declaring winning conditions
+const playerWon = () => {
+    if (spaces[0] === currentPlayer) {
+        if (spaces[1] === currentPlayer && spaces[2] === currentPlayer) {
+            strategy.innerText = `${currentPlayer} wins up to top`;
+            return true;
+        }
+        if (spaces[3] === currentPlayer && spaces[6] === currentPlayer) {
+            strategy.innerText = `${currentPlayer} wins on the left`;
+            return true;
+        }
+        if (spaces[4] === currentPlayer && spaces[8] === currentPlayer) {
+            strategy.innerText = `${currentPlayer} wins diagonally`;
+            return true;
+        }
+    }
+    if (spaces[8] === currentPlayer) {
+        if (spaces[2] === currentPlayer && spaces[5] === currentPlayer) {
+            strategy.innerText = `${currentPlayer} wins on the right`;
+            return true;
+        }
+        if (spaces[6] === currentPlayer && spaces[7] === currentPlayer) {
+            strategy.innerText = `${currentPlayer} wins on the bottom`;
+            return true;
+        }
+    }
+    if (spaces[4] === currentPlayer) {
+        if (spaces[1] === currentPlayer && spaces[7] === currentPlayer) {
+            strategy.innerText = `${currentPlayer} wins vertically on middle`;
+            return true;
+        }
+        if (spaces[3] === currentPlayer && spaces[5] === currentPlayer) {
+            strategy.innerText = `${currentPlayer} wins horizontally on the middle`;
+            return true;
+        }
+        if (spaces[2] === currentPlayer && spaces[6] === currentPlayer) {
+            strategy.innerText = `${currentPlayer} wins diagonally`;
+            return true;
+        }
+    }
+};
+
+//Delcaring draw conditions
+const playerDraw = () => {
+    let draw = 0;
+    spaces.forEach((space, i) => {
+        if (spaces[i] !== null) draw++;
+    });
+    if (draw === 9) {
+        text.innerText = `Draw`;
+        restart();
+    }
+};
+
+//The restart function
+const restart = () => {
+    setTimeout(() => {
+        spaces.forEach((space, i) => {
+            spaces[i] = null;
+        });
+        boxes.forEach((box) => {
+            box.innerText = '';
+        });
+        text.innerText = `Play`;
+        strategy.innerText = ``;
+    }, 1000);
+};
+
